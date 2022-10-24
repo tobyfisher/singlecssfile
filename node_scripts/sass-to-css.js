0d57a3ddb1bd35b413e127e749a297c8a3ab4add
Toby Fisher <toby@knowego.com>
2022-10-24 10:29:26 +0100

    Fixes for clinic and oescape layouts

--- node_scripts/sass-to-css.js
+++ node_scripts/sass-to-css.js
@@ -60,11 +60,9 @@ const headerLegals = [
 	'* You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see www.gnu.org/licenses.',
 	'*',
 	'* @link www.openeyes.org.uk',
-	'*',
 	'* @author OpenEyes info@openeyes.org.uk',
 	'* @copyright Copyright (C) 2018, OpenEyes Foundation',
 	'* @license www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0',
-	'*',
 	'*/',
 	'', '' ].join('\n');
 
